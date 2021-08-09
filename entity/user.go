600fdf3c9662ec5cabfa12b807d52cd4b77719ad
7kaji <1157309+7kaji@users.noreply.github.com>
2021-08-09 18:24:38 +0900

    rename the json key

--- entity/user.go
+++ entity/user.go
@@ -3,6 +3,6 @@ package entity
 // User is user models property
 type User struct {
 	ID        uint   `db:"id" json:"id"`
-	FirstName string `db:"first_name" json:"firstname"`
-	LastName  string `db:"last_name" json:"lastname"`
+	FirstName string `db:"first_name" json:"first_name"`
+	LastName  string `db:"last_name" json:"last_name"`
 }
